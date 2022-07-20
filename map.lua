a10e18bd72921c5dd4d51c7e234582cb6ce704dd
scoutproduction <59487359+scoutproduction@users.noreply.github.com>
2022-07-20 08:55:34 +0400

    Create map.lua

---
+++ map.lua
@@ -0,0 +1,6 @@
+return {
+  ['Bot'] = "RoEx-Bot/Bot/Bot Class.lua"
+  ['Pet Simulator X'] ="RoEx-Bot/Bot/Classes/Pet Simulator X.lua"
+  ['Adopt Me'] = "RoEx-Bot/Bot/Classes/Adopt Me.lua"
+  ['Bee Simulator']= "RoEx-Bot/Bot/Classes/Bee Simulator.lua"
+}
