45a678094f993d1cd5ff68a08b0257cd2e1d8723
scoutproduction <59487359+scoutproduction@users.noreply.github.com>
2022-07-20 08:41:32 +0400

    Update Manager.lua

--- Manager.lua
+++ Manager.lua
@@ -1,1 +1,35 @@
 
+function CreateBot(gameName,userName,itemName)
+	local bot = require(script.Parent.bot)
+	
+	local function GetModule()
+		for _, instance in pairs(script.Parent.classes.modules:GetChildren()) do 
+			if string.find(string.upper(gameName), instance.Name) then 
+				return instance
+			end 
+		end 
+	end 
+
+	bot:LoadData({  -- INTIAL DATA 
+		GAME_NAME = gameName;
+		USER_NAME = userName;
+		ITEM_NAME = itemName; 
+		MODULE_NAME = GetModule().Name; 
+	}) 
+
+
+	if bot.DATA.MODULE_NAME then 
+		print("Assigning Class") 
+		local class = require(bot.DATA.MODULE)
+		bot:LoadData({Class = class.Create(bot)})
+		if bot.DATA.MODULE then 
+			bot:IntiateTrade()
+		end
+	end 
+end 
+
+CreateBot("Pet Simulator X", "", "")
+
+CreateBot("Adopt Me", "", "")
+
+
