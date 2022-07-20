7b525e6984576b65b537284bfa18a0fe0487abcb
scoutproduction <59487359+scoutproduction@users.noreply.github.com>
2022-07-20 08:42:51 +0400

    Update template.lua

--- Bot/Classes/template.lua
+++ Bot/Classes/template.lua
@@ -1,1 +1,30 @@
 
+local Class = {}
+Class.__index = Class 
+
+function Class.Create(Bot)
+	local intialData = {
+		PROXY = Bot.GenerateProxyTable({}, Class.Track())
+	}
+	local class = setmetatable(intialData,Class)
+	return class 
+end 
+
+function Class.Track(t,k,v)
+
+	print("TABLE: ", t)
+	print("INDEX : ",k)
+	print("VALUE : ", v)
+
+
+end 
+
+function Class:IntiateTrade()
+ 
+end
+
+
+
+
+function Class
+
